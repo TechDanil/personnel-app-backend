536a7cb0754a5120d9b96ef911b7ee51479ab748
TechDanil <danilkomarov243242@gmail.com>
2023-08-23 16:15:09 +0500

    renamed db in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'postgres',
+      database: 'employeesDB',
     }),
   ],
 })
